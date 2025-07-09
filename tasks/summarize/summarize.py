4c251f2295622a9dd3ebfd4bfe1db9e12d2609c2
Daniel Fernández <daniel.fdez.fdez@gmail.com>
2025-07-09 12:56:40 +0200

    Refactor summarize_text function to use 'payload' parameter instead of 'job'

--- tasks/summarize/summarize.py
+++ tasks/summarize/summarize.py
@@ -4,7 +4,7 @@ import re
 
 
 @job_handler("summarize")
-def summarize_text(job) -> dict:
+def summarize_text(payload) -> dict:
     model_name = "facebook/mbart-large-50-one-to-many-mmt"
 
     try:
@@ -13,10 +13,10 @@ def summarize_text(job) -> dict:
         tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
 
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    tokenizer.src_lang = job["payload"]["sourceLanguage"] + "_XX"
-    es_forced_bos_token_id = tokenizer.lang_code_to_id[job["payload"]["targetLanguage"] + "_XX"]
+    tokenizer.src_lang = payload["sourceLanguage"] + "_XX"
+    es_forced_bos_token_id = tokenizer.lang_code_to_id[payload["targetLanguage"] + "_XX"]
 
-    plain_text = re.sub(r"<[^>]+>", "", job["payload"]["content"])
+    plain_text = re.sub(r"<[^>]+>", "", payload["content"])
 
     inputs = tokenizer(
         plain_text,
